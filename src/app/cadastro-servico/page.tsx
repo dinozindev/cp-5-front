3a8b4e0386e8a8919548891940f9fd41da4120b2
Lucas <124621971+dinozindev@users.noreply.github.com>
2024-10-17 08:08:35 -0300

    Add files via upload

--- src/app/cadastro-servico/page.tsx
+++ src/app/cadastro-servico/page.tsx
@@ -13,61 +13,61 @@ const schema = yup.object().shape({
 
 const CadastroServicoPage = () => {
     const [listaServicos, setListaServicos] = useState<any[]>([]);
-    
+
     const router = useRouter();
 
     useEffect(() => {
         const logado = sessionStorage.getItem('logado');
         if (!logado || logado === 'false') {
-            router.push('/acesso-negado'); 
+            router.push('/acesso-negado');
         }
         const servicos = localStorage.getItem('servicos');
         const servicosObtidos = servicos == null ? [] : JSON.parse(servicos);
         setListaServicos(servicosObtidos);
     }, []);
 
-    const {register, handleSubmit, formState : {errors}} = useForm({resolver: yupResolver(schema)})
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) })
 
-    const inserirServico = (servico : any) => {
+    const inserirServico = (servico: any) => {
         const listaAtualizada = [...listaServicos, servico]
         setListaServicos(listaAtualizada)
         localStorage.setItem("servicos", JSON.stringify(listaAtualizada))
         router.push('/cadastro-concluido')
     }
 
-  return (
-    <>
-    <h1 className="page__title">Agendar um Serviço</h1>
-    <form onSubmit={handleSubmit(inserirServico)} className="form__cadastro--service">
-        <label>Tipo do Serviço
-            <select {...register('nome')} defaultValue={'DEFAULT'}>
-            <option value="DEFAULT" disabled>Selecione o tipo de Serviço...</option>
-            <option value="Banho">Banho</option>
-            <option value="Tosa Completa">Tosa Completa</option>
-            <option value="Tosa Higiênica">Tosa Higiênica</option>
-            <option value="Consulta Veterinária">Consulta Veterinária</option>
-            <option value="Hotel Pet">Hotel Pet</option>
-            <option value="Daycare (Creche)">Daycare (Creche)</option>
-            <option value="Adestramento">Adestramento</option>
-            <option value="Transporte do Pet">Transporte do Pet</option>
-            </select>
-            <span className="input__error">{errors.nome?.message}</span>
-        </label>
-        <br/>
-        <label>Data do Serviço
-            <input type="datetime-local" {...register('data')}/>
-            <span className="input__error">{errors.data?.message}</span>
-        </label>
-        <br />
-        <label>Observações
-            <input type="text" {...register('observacao')}/>
-            <span className="input__error">{errors.observacao?.message}</span>
-        </label>
-        <br />
-        <input type="submit" className="input__submit" />
-    </form>
-    </>
-  )
+    return (
+        <>
+            <h1 className="page__title">Agendar um Serviço</h1>
+            <form onSubmit={handleSubmit(inserirServico)} className="form__cadastro--service">
+                <label>Tipo do Serviço
+                    <select {...register('nome')} defaultValue={'DEFAULT'}>
+                        <option value="DEFAULT" disabled>Selecione o tipo de Serviço...</option>
+                        <option value="Banho">Banho</option>
+                        <option value="Tosa Completa">Tosa Completa</option>
+                        <option value="Tosa Higiênica">Tosa Higiênica</option>
+                        <option value="Consulta Veterinária">Consulta Veterinária</option>
+                        <option value="Hotel Pet">Hotel Pet</option>
+                        <option value="Daycare (Creche)">Daycare (Creche)</option>
+                        <option value="Adestramento">Adestramento</option>
+                        <option value="Transporte do Pet">Transporte do Pet</option>
+                    </select>
+                    <span className="input__error">{errors.nome?.message}</span>
+                </label>
+                <br />
+                <label>Data do Serviço
+                    <input type="datetime-local" {...register('data')} />
+                    <span className="input__error">{errors.data?.message}</span>
+                </label>
+                <br />
+                <label>Observações
+                    <input type="text" {...register('observacao')} />
+                    <span className="input__error">{errors.observacao?.message}</span>
+                </label>
+                <br />
+                <input type="submit" className="input__submit" />
+            </form>
+        </>
+    )
 }
 
 export default CadastroServicoPage
